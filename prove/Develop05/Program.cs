7cae67f00e6159a2ae600b58ecbfa14a09c032ee
kdabums <cybercultureinternational@gmail.com>
2024-02-10 20:23:27 +0100

    add

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,79 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        int userInput;
+        GoalManager goalManager = new GoalManager();
+        goalManager.DisplayPlayerInfo();
+        Console.Write("");
+       
+
+        do
+        {
+            goalManager.Start();
+            Console.Write("Select a menu of your choice");
+            userInput = int.Parse(Console.ReadLine());
+            if (userInput == 1)
+            {
+                goalManager.CreateGoals();                
+                
+            }
+            else if (userInput == 2)
+            {
+                goalManager.GoalDetails();
+                
+            }
+            else if (userInput == 3)
+            {
+                goalManager.SaveGoals();
+                
+            }
+            else if (userInput == 4)
+            {
+                goalManager.LoadGoals();
+
+            }
+            else if (userInput == 5)
+            {
+                goalManager.RecordEvent();   
+                                
+                
+            }
+          
+        } while (userInput!= 6);
+
+        // while (userInput!= 6)
+        // {
+        //     if (userInput == 1)
+        //     {
+        //         goalManager.CreateGoals();                
+                
+        //     }
+        //     else if (userInput == 2)
+        //     {
+        //         goalManager.GoalDetails();
+                
+        //     }
+        //     else if (userInput == 3)
+        //     {
+        //         goalManager.SaveGoals();
+                
+        //     }
+        //     else if (userInput == 4)
+        //     {
+        //         goalManager.LoadGoals();
+
+        //     }
+        //     else if (userInput == 5)
+        //     {
+        //         goalManager.RecordEvent();   
+                                
+                
+        //     }
+        //     else
+        //     {
+        //         return;
+        //     }
+            
+        // }
     }
 }
